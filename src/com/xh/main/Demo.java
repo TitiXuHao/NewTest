1ac47a21face207805ce78001fcecc703e17ffa6
XuHao <897247627@qq.com>
2019-07-01 17:02:33 +0800

    update Demo.java

--- src/com/xh/main/Demo.java
+++ src/com/xh/main/Demo.java
@@ -4,5 +4,9 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
+		
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(i);
+		}
 	}
 }
